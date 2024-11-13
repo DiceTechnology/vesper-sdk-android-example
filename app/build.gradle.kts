57d68c92e505588879fbeadd3d7818100e08a5d1
Arnold Szabo <szaboaarnold@gmail.com>
2024-11-13 14:13:06 +0100

    Bump vesper-sdk-android to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
     // Vesper SDK
-    implementation("com.github.DiceTechnology:vesper-sdk-android:1.0.4") {
+    implementation("com.github.DiceTechnology:vesper-sdk-android:1.0.6") {
         exclude(group = "androidx.media3")
     }
     implementation(libs.androidx.core.ktx)
