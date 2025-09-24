a917cf5d3da20b04e80f16bd018d0d2a0bcbca36
Arnold Szabo <szaboaarnold@gmail.com>
2025-09-24 17:09:21 +0200

    chore: bump sdk to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
     // Vesper SDK
-    implementation("com.endeavorstreaming.vesper:vesper-sdk:2.0.6") {
+    implementation("com.endeavorstreaming.vesper:vesper-sdk:2.1.1") {
         exclude(group = "androidx.media3")
     }
     implementation(libs.androidx.core.ktx)
