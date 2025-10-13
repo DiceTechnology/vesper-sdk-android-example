60bd28e440dbf602bd22147205d0cef1b61f5d1c
Arnold Szabó <szaboaarnold@gmail.com>
2025-10-13 11:21:53 +0200

    fix: enable pip on player manager config (#15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
     // Vesper SDK
-    implementation("com.endeavorstreaming.vesper:vesper-sdk:2.1.2") {
+    implementation("com.endeavorstreaming.vesper:vesper-sdk:2.1.7") {
         exclude(group = "androidx.media3")
     }
     implementation(libs.androidx.core.ktx)
